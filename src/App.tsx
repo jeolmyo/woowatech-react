a47dda6d3720ccade31f264dff83ec3dd2b419cf
jeolmyo <cinabro88@gmail.com>
2019-05-18 20:20:29 +0900

    v1 완성.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { MonitorCard, PlayButton } from "./components";
+import { Typography } from "antd";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "antd/dist/antd.css";
+import "./sass/main.scss";
+import { setInterval } from "timers";
+
+interface Application {
+  timerId: number;
+  state: {
+    success: number;
+    failure: number;
+  };
+  onStart(): void;
+  onStop(): void;
 }
 
-export default App;
+export default class App extends React.Component implements Application {
+  timerId: number = 0;
+
+  state = {
+    success: 0,
+    failure: 0
+  };
+
+  onStart = () => {
+    
+    this.timerId = window.setInterval(() => {
+      this.setState({
+        success: this.state.success + Math.floor(Math.random() * (100 - 1) + 1),
+        failure: this.state.failure + Math.floor(Math.random() * 2 - 0)
+      });
+    }, 200);
+  };
+
+  onStop = () => {
+    clearInterval(this.timerId);
+    this.timerId = 0;
+  };
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Typography.Title>React & TS Boilerplate</Typography.Title>
+          <Typography>Order Monitor</Typography>
+        </header>
+        <main>
+          <MonitorCard
+            success={this.state.success}
+            failure={this.state.failure}
+          />
+          <PlayButton
+            monitoring={false}
+            onPlay={this.onStart}
+            onPause={this.onStop}
+          />
+        </main>
+      </div>
+    );
+  }
+}
